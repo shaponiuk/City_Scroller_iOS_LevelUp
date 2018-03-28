98fde7326a8c76746d3272757d0e9e0be6ea8694
Szymon Haponiuk <shaponiuk@szymons-macbook-pro.local>
2018-03-28 15:11:07 +0200

    Initial Commit

---
+++ ScrollApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollApp
+//
+//  Created by Szymon Haponiuk on 28/03/2018.
+//  Copyright © 2018 Szymon Haponiuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
